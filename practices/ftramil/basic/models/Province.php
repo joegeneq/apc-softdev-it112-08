233c960b45feeae50d5b04d5dc85b182bbd3adfc
Aeya Ramil <aeya.ramil@gmail.com>
2015-02-20 21:55:43 +0800

    Practice database IT112_Exercise1

---
+++ practices/ftramil/basic/models/Province.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "province".
+ *
+ * @property integer $province_id
+ * @property string $province_code
+ * @property string $province_description
+ * @property integer $region_id
+ *
+ * @property City[] $cities
+ * @property Region $region
+ */
+class Province extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'province';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['region_id'], 'required'],
+            [['region_id'], 'integer'],
+            [['province_code', 'province_description'], 'string', 'max' => 32]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'province_id' => 'Province ID',
+            'province_code' => 'Province Code',
+            'province_description' => 'Province Description',
+            'region_id' => 'Region ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCities()
+    {
+        return $this->hasMany(City::className(), ['province_id' => 'province_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRegion()
+    {
+        return $this->hasOne(Region::className(), ['region_id' => 'region_id']);
+    }
+}
---
+++ practices/ftramil/basic/views/province/view.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Province */
+
+$this->title = $model->province_id;
+$this->params['breadcrumbs'][] = ['label' => 'Provinces', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="province-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'province_id' => $model->province_id, 'region_id' => $model->region_id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'province_id' => $model->province_id, 'region_id' => $model->region_id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'province_id',
+            'province_code',
+            'province_description',
+            'region_id',
+        ],
+    ]) ?>
+
+</div>
