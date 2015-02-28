4524f6f6497fc94edcd7bf88c5dd7e1f74140b5d
Aeya Ramil <aeya.ramil@gmail.com>
2015-02-28 15:15:25 +0800

    Insert one record for myaddress and mycomment tables and overwrite .sql file

--- practices/midterm_test/Ramil_Aeya_midterms/softdev_Ramil_FrancesAeya_ourspace.sql
+++ practices/midterm_test/Ramil_Aeya_midterms/softdev_Ramil_FrancesAeya_ourspace.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 28, 2015 at 07:42 AM
+-- Generation Time: Feb 28, 2015 at 08:13 AM
 -- Server version: 5.5.25a
 -- PHP Version: 5.4.4
 
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `myaddress` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(30) NOT NULL,
   `middlename` varchar(30) NOT NULL,
   `lastname` varchar(30) NOT NULL,
@@ -37,7 +37,14 @@ CREATE TABLE IF NOT EXISTS `myaddress` (
   `landline` varchar(20) NOT NULL,
   `cellphone` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `myaddress`
+--
+
+INSERT INTO `myaddress` (`id`, `firstname`, `middlename`, `lastname`, `gender`, `created_at`, `home_address`, `landline`, `cellphone`) VALUES
+(1, 'Jocelyn', 'Alfaro', 'Cacao', 'F', '2015-02-28 07:05:02', 'Marcelo Green Village, Paranaque City', '8211588', '09258001234');
 
 -- --------------------------------------------------------
 
@@ -46,14 +53,21 @@ CREATE TABLE IF NOT EXISTS `myaddress` (
 --
 
 CREATE TABLE IF NOT EXISTS `mycomment` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `myaddress_id` int(11) NOT NULL,
   `author` varchar(255) NOT NULL,
   `body` longtext NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `mycomment_ibfk_1` (`myaddress_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `mycomment`
+--
+
+INSERT INTO `mycomment` (`id`, `myaddress_id`, `author`, `body`, `created_at`) VALUES
+(1, 1, 'Jose Eugenio Quesada', 'SOFTDEV-IT112 Midterm Exam', '2015-02-28 07:12:18');
 
 --
 -- Constraints for dumped tables
