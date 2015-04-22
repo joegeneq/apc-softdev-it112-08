d6eec94d8da8bb373116a00aa18f884e4c469848
Aeya Ramil <aeya.ramil@gmail.com>
2015-04-22 16:42:49 +0800

    generate models and controllers for design, instruction and acuity tables

---
+++ application/ecpms-sd/backend/models/Design.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "design".
+ *
+ * @property integer $design_id
+ * @property string $design_name
+ * @property string $design_description
+ *
+ * @property Prescription[] $prescriptions
+ */
+class Design extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'design';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['design_name'], 'required'],
+            [['design_name', 'design_description'], 'string', 'max' => 45],
+            [['design_name'], 'unique']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'design_id' => 'Design ID',
+            'design_name' => 'Design Name',
+            'design_description' => 'Design Description',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPrescriptions()
+    {
+        return $this->hasMany(Prescription::className(), ['design_id' => 'design_id']);
+    }
+}
