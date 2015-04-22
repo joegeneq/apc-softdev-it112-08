6c1f10bde69307e2ec4793ecdeaf9fe0810dbce6
Jocelyn Cacao <joycacao@gmail.com>
2015-04-22 17:03:43 +0800

    evaluation and solutions generated (controller, models, views)

---
+++ application/ecpms-sd/frontend/models/EvaluationSearch.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use frontend\models\Evaluation;
+
+/**
+ * EvaluationSearch represents the model behind the search form about `frontend\models\Evaluation`.
+ */
+class EvaluationSearch extends Evaluation
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['record_id', 'v_d_od_sc', 'v_d_od_cc', 'v_d_od_ph', 'v_n_od_sc', 'v_n_od_cc', 'v_d_os_sc', 'v_d_os_cc', 'v_d_os_ph', 'v_n_os_sc', 'v_n_os_cc', 'v_d_ou_sc', 'v_d_ou_cc', 'v_d_ou_ph', 'v_n_ou_sc', 'v_n_ou_cc', 'k_h_od', 'k_h_os', 'k_v_od', 'k_v_os', 'k_c_od', 'k_c_os', 'k_a_od', 'k_a_os', 't_od', 't_os', 'bp_s', 'bp_d', 'p_d_l', 'p_d_v', 'p_n_l', 'p_n_v', 'p_d_bi', 'p_d_lbu', 'p_n_bi', 'p_n_lbu', 'p_d_bo', 'p_d_lbo', 'p_n_bo', 'p_n_lbo', 'eoms_id', 'm_eoms_id', 'm_p_accom', 'm_a_accom', 'm_p_conver_num', 'm_p_conver_den', 'f_me_od', 'f_me_os', 'f_d_od', 'f_d_os', 'f_c_od', 'f_c_os'], 'integer'],
+            [['r_a_od', 'r_a_os', 'r_m_od', 'r_m_os', 'r_c_od', 'r_c_os', 'r_o_od', 'r_o_os', 'r_w_od', 'r_w_os'], 'number'],
+            [['pupil_p_od', 'pupil_p_os', 'pupil_s_od', 'pupil_s_os', 'pupil_rapd', 'm_dominant', 'm_other', 'color_test', 'stereo_w4d', 'confrontation_avf', 'gross_exam', 'ee_lids_od', 'ee_lids_os', 'ee_c_od', 'ee_c_os', 'ee_d_od', 'ee_d_os', 'ee_l_od', 'ee_l_os', 'ee_t_od', 'ee_t_os', 'ee_s_od', 'ee_s_os', 'ee_od', 'ee_os', 'se_c_od', 'se_c_os', 'se_a_od', 'se_a_os', 'se_i_od', 'se_i_os', 'se_l_od', 'se_l_os', 'se_v_od', 'se_v_os', 'se_od', 'se_os', 'f_v_od', 'f_v_os', 'f_p_od', 'f_p_os', 'f_ma_od', 'f_m_os'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Evaluation::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'record_id' => $this->record_id,
+            'v_d_od_sc' => $this->v_d_od_sc,
+            'v_d_od_cc' => $this->v_d_od_cc,
+            'v_d_od_ph' => $this->v_d_od_ph,
+            'v_n_od_sc' => $this->v_n_od_sc,
+            'v_n_od_cc' => $this->v_n_od_cc,
+            'v_d_os_sc' => $this->v_d_os_sc,
+            'v_d_os_cc' => $this->v_d_os_cc,
+            'v_d_os_ph' => $this->v_d_os_ph,
+            'v_n_os_sc' => $this->v_n_os_sc,
+            'v_n_os_cc' => $this->v_n_os_cc,
+            'v_d_ou_sc' => $this->v_d_ou_sc,
+            'v_d_ou_cc' => $this->v_d_ou_cc,
+            'v_d_ou_ph' => $this->v_d_ou_ph,
+            'v_n_ou_sc' => $this->v_n_ou_sc,
+            'v_n_ou_cc' => $this->v_n_ou_cc,
+            'r_a_od' => $this->r_a_od,
+            'r_a_os' => $this->r_a_os,
+            'r_m_od' => $this->r_m_od,
+            'r_m_os' => $this->r_m_os,
+            'r_c_od' => $this->r_c_od,
+            'r_c_os' => $this->r_c_os,
+            'r_o_od' => $this->r_o_od,
+            'r_o_os' => $this->r_o_os,
+            'r_w_od' => $this->r_w_od,
+            'r_w_os' => $this->r_w_os,
+            'k_h_od' => $this->k_h_od,
+            'k_h_os' => $this->k_h_os,
+            'k_v_od' => $this->k_v_od,
+            'k_v_os' => $this->k_v_os,
+            'k_c_od' => $this->k_c_od,
+            'k_c_os' => $this->k_c_os,
+            'k_a_od' => $this->k_a_od,
+            'k_a_os' => $this->k_a_os,
+            't_od' => $this->t_od,
+            't_os' => $this->t_os,
+            'bp_s' => $this->bp_s,
+            'bp_d' => $this->bp_d,
+            'p_d_l' => $this->p_d_l,
+            'p_d_v' => $this->p_d_v,
+            'p_n_l' => $this->p_n_l,
+            'p_n_v' => $this->p_n_v,
+            'p_d_bi' => $this->p_d_bi,
+            'p_d_lbu' => $this->p_d_lbu,
+            'p_n_bi' => $this->p_n_bi,
+            'p_n_lbu' => $this->p_n_lbu,
+            'p_d_bo' => $this->p_d_bo,
+            'p_d_lbo' => $this->p_d_lbo,
+            'p_n_bo' => $this->p_n_bo,
+            'p_n_lbo' => $this->p_n_lbo,
+            'eoms_id' => $this->eoms_id,
+            'm_eoms_id' => $this->m_eoms_id,
+            'm_p_accom' => $this->m_p_accom,
+            'm_a_accom' => $this->m_a_accom,
+            'm_p_conver_num' => $this->m_p_conver_num,
+            'm_p_conver_den' => $this->m_p_conver_den,
+            'f_me_od' => $this->f_me_od,
+            'f_me_os' => $this->f_me_os,
+            'f_d_od' => $this->f_d_od,
+            'f_d_os' => $this->f_d_os,
+            'f_c_od' => $this->f_c_od,
+            'f_c_os' => $this->f_c_os,
+        ]);
+
+        $query->andFilterWhere(['like', 'pupil_p_od', $this->pupil_p_od])
+            ->andFilterWhere(['like', 'pupil_p_os', $this->pupil_p_os])
+            ->andFilterWhere(['like', 'pupil_s_od', $this->pupil_s_od])
+            ->andFilterWhere(['like', 'pupil_s_os', $this->pupil_s_os])
+            ->andFilterWhere(['like', 'pupil_rapd', $this->pupil_rapd])
+            ->andFilterWhere(['like', 'm_dominant', $this->m_dominant])
+            ->andFilterWhere(['like', 'm_other', $this->m_other])
+            ->andFilterWhere(['like', 'color_test', $this->color_test])
+            ->andFilterWhere(['like', 'stereo_w4d', $this->stereo_w4d])
+            ->andFilterWhere(['like', 'confrontation_avf', $this->confrontation_avf])
+            ->andFilterWhere(['like', 'gross_exam', $this->gross_exam])
+            ->andFilterWhere(['like', 'ee_lids_od', $this->ee_lids_od])
+            ->andFilterWhere(['like', 'ee_lids_os', $this->ee_lids_os])
+            ->andFilterWhere(['like', 'ee_c_od', $this->ee_c_od])
+            ->andFilterWhere(['like', 'ee_c_os', $this->ee_c_os])
+            ->andFilterWhere(['like', 'ee_d_od', $this->ee_d_od])
+            ->andFilterWhere(['like', 'ee_d_os', $this->ee_d_os])
+            ->andFilterWhere(['like', 'ee_l_od', $this->ee_l_od])
+            ->andFilterWhere(['like', 'ee_l_os', $this->ee_l_os])
+            ->andFilterWhere(['like', 'ee_t_od', $this->ee_t_od])
+            ->andFilterWhere(['like', 'ee_t_os', $this->ee_t_os])
+            ->andFilterWhere(['like', 'ee_s_od', $this->ee_s_od])
+            ->andFilterWhere(['like', 'ee_s_os', $this->ee_s_os])
+            ->andFilterWhere(['like', 'ee_od', $this->ee_od])
+            ->andFilterWhere(['like', 'ee_os', $this->ee_os])
+            ->andFilterWhere(['like', 'se_c_od', $this->se_c_od])
+            ->andFilterWhere(['like', 'se_c_os', $this->se_c_os])
+            ->andFilterWhere(['like', 'se_a_od', $this->se_a_od])
+            ->andFilterWhere(['like', 'se_a_os', $this->se_a_os])
+            ->andFilterWhere(['like', 'se_i_od', $this->se_i_od])
+            ->andFilterWhere(['like', 'se_i_os', $this->se_i_os])
+            ->andFilterWhere(['like', 'se_l_od', $this->se_l_od])
+            ->andFilterWhere(['like', 'se_l_os', $this->se_l_os])
+            ->andFilterWhere(['like', 'se_v_od', $this->se_v_od])
+            ->andFilterWhere(['like', 'se_v_os', $this->se_v_os])
+            ->andFilterWhere(['like', 'se_od', $this->se_od])
+            ->andFilterWhere(['like', 'se_os', $this->se_os])
+            ->andFilterWhere(['like', 'f_v_od', $this->f_v_od])
+            ->andFilterWhere(['like', 'f_v_os', $this->f_v_os])
+            ->andFilterWhere(['like', 'f_p_od', $this->f_p_od])
+            ->andFilterWhere(['like', 'f_p_os', $this->f_p_os])
+            ->andFilterWhere(['like', 'f_ma_od', $this->f_ma_od])
+            ->andFilterWhere(['like', 'f_m_os', $this->f_m_os]);
+
+        return $dataProvider;
+    }
+}
---
+++ application/ecpms-sd/frontend/views/evaluation/_form.php
@@ -0,0 +1,231 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\Evaluation */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="evaluation-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'record_id')->textInput() ?>
+
+    <?= $form->field($model, 'v_d_od_sc')->textInput() ?>
+
+    <?= $form->field($model, 'v_d_od_cc')->textInput() ?>
+
+    <?= $form->field($model, 'v_d_od_ph')->textInput() ?>
+
+    <?= $form->field($model, 'v_n_od_sc')->textInput() ?>
+
+    <?= $form->field($model, 'v_n_od_cc')->textInput() ?>
+
+    <?= $form->field($model, 'v_d_os_sc')->textInput() ?>
+
+    <?= $form->field($model, 'v_d_os_cc')->textInput() ?>
+
+    <?= $form->field($model, 'v_d_os_ph')->textInput() ?>
+
+    <?= $form->field($model, 'v_n_os_sc')->textInput() ?>
+
+    <?= $form->field($model, 'v_n_os_cc')->textInput() ?>
+
+    <?= $form->field($model, 'v_d_ou_sc')->textInput() ?>
+
+    <?= $form->field($model, 'v_d_ou_cc')->textInput() ?>
+
+    <?= $form->field($model, 'v_d_ou_ph')->textInput() ?>
+
+    <?= $form->field($model, 'v_n_ou_sc')->textInput() ?>
+
+    <?= $form->field($model, 'v_n_ou_cc')->textInput() ?>
+
+    <?= $form->field($model, 'r_a_od')->textInput(['maxlength' => 5]) ?>
+
+    <?= $form->field($model, 'r_a_os')->textInput(['maxlength' => 5]) ?>
+
+    <?= $form->field($model, 'r_m_od')->textInput(['maxlength' => 5]) ?>
+
+    <?= $form->field($model, 'r_m_os')->textInput(['maxlength' => 5]) ?>
+
+    <?= $form->field($model, 'r_c_od')->textInput(['maxlength' => 5]) ?>
+
+    <?= $form->field($model, 'r_c_os')->textInput(['maxlength' => 5]) ?>
+
+    <?= $form->field($model, 'r_o_od')->textInput(['maxlength' => 5]) ?>
+
+    <?= $form->field($model, 'r_o_os')->textInput(['maxlength' => 5]) ?>
+
+    <?= $form->field($model, 'r_w_od')->textInput(['maxlength' => 5]) ?>
+
+    <?= $form->field($model, 'r_w_os')->textInput(['maxlength' => 5]) ?>
+
+    <?= $form->field($model, 'k_h_od')->textInput() ?>
+
+    <?= $form->field($model, 'k_h_os')->textInput() ?>
+
+    <?= $form->field($model, 'k_v_od')->textInput() ?>
+
+    <?= $form->field($model, 'k_v_os')->textInput() ?>
+
+    <?= $form->field($model, 'k_c_od')->textInput() ?>
+
+    <?= $form->field($model, 'k_c_os')->textInput() ?>
+
+    <?= $form->field($model, 'k_a_od')->textInput() ?>
+
+    <?= $form->field($model, 'k_a_os')->textInput() ?>
+
+    <?= $form->field($model, 't_od')->textInput() ?>
+
+    <?= $form->field($model, 't_os')->textInput() ?>
+
+    <?= $form->field($model, 'bp_s')->textInput() ?>
+
+    <?= $form->field($model, 'bp_d')->textInput() ?>
+
+    <?= $form->field($model, 'p_d_l')->textInput() ?>
+
+    <?= $form->field($model, 'p_d_v')->textInput() ?>
+
+    <?= $form->field($model, 'p_n_l')->textInput() ?>
+
+    <?= $form->field($model, 'p_n_v')->textInput() ?>
+
+    <?= $form->field($model, 'p_d_bi')->textInput() ?>
+
+    <?= $form->field($model, 'p_d_lbu')->textInput() ?>
+
+    <?= $form->field($model, 'p_n_bi')->textInput() ?>
+
+    <?= $form->field($model, 'p_n_lbu')->textInput() ?>
+
+    <?= $form->field($model, 'p_d_bo')->textInput() ?>
+
+    <?= $form->field($model, 'p_d_lbo')->textInput() ?>
+
+    <?= $form->field($model, 'p_n_bo')->textInput() ?>
+
+    <?= $form->field($model, 'p_n_lbo')->textInput() ?>
+
+    <?= $form->field($model, 'pupil_p_od')->textInput(['maxlength' => 1]) ?>
+
+    <?= $form->field($model, 'pupil_p_os')->textInput(['maxlength' => 1]) ?>
+
+    <?= $form->field($model, 'pupil_s_od')->textInput(['maxlength' => 1]) ?>
+
+    <?= $form->field($model, 'pupil_s_os')->textInput(['maxlength' => 1]) ?>
+
+    <?= $form->field($model, 'pupil_rapd')->textInput(['maxlength' => 1]) ?>
+
+    <?= $form->field($model, 'eoms_id')->textInput() ?>
+
+    <?= $form->field($model, 'm_eoms_id')->textInput() ?>
+
+    <?= $form->field($model, 'm_dominant')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'm_other')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'm_p_accom')->textInput() ?>
+
+    <?= $form->field($model, 'm_a_accom')->textInput() ?>
+
+    <?= $form->field($model, 'm_p_conver_num')->textInput() ?>
+
+    <?= $form->field($model, 'm_p_conver_den')->textInput() ?>
+
+    <?= $form->field($model, 'color_test')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'stereo_w4d')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'confrontation_avf')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'gross_exam')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'ee_lids_od')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'ee_lids_os')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'ee_c_od')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'ee_c_os')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'ee_d_od')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'ee_d_os')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'ee_l_od')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'ee_l_os')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'ee_t_od')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'ee_t_os')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'ee_s_od')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'ee_s_os')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'ee_od')->textInput() ?>
+
+    <?= $form->field($model, 'ee_os')->textInput() ?>
+
+    <?= $form->field($model, 'se_c_od')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'se_c_os')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'se_a_od')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'se_a_os')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'se_i_od')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'se_i_os')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'se_l_od')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'se_l_os')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'se_v_od')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'se_v_os')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'se_od')->textInput() ?>
+
+    <?= $form->field($model, 'se_os')->textInput() ?>
+
+    <?= $form->field($model, 'f_me_od')->textInput() ?>
+
+    <?= $form->field($model, 'f_me_os')->textInput() ?>
+
+    <?= $form->field($model, 'f_d_od')->textInput() ?>
+
+    <?= $form->field($model, 'f_d_os')->textInput() ?>
+
+    <?= $form->field($model, 'f_c_od')->textInput() ?>
+
+    <?= $form->field($model, 'f_c_os')->textInput() ?>
+
+    <?= $form->field($model, 'f_v_od')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'f_v_os')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'f_p_od')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'f_p_os')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'f_ma_od')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'f_m_os')->textInput(['maxlength' => 45]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
