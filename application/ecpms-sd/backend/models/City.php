13326a714f9a6736b7b1ddeeb3d09ebe7e737aa2
Francis Horca <fdhorca@gmail.com>
2015-04-23 21:35:23 -0700

    generated crud on backend and frontend

---
+++ application/ecpms-sd/backend/models/City.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "city".
+ *
+ * @property integer $city_id
+ * @property string $city_name
+ * @property integer $city_code
+ *
+ * @property User[] $users
+ */
+class City extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'city';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['city_name'], 'required'],
+            [['city_code'], 'integer'],
+            [['city_name'], 'string', 'max' => 45],
+            [['city_name'], 'unique']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'city_id' => 'City ID',
+            'city_name' => 'City Name',
+            'city_code' => 'City Code',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUsers()
+    {
+        return $this->hasMany(User::className(), ['city_city_id' => 'city_id']);
+    }
+}
---
+++ application/ecpms-sd/backend/models/Insurance.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "insurance".
+ *
+ * @property integer $insurance_id
+ * @property string $insurance_name
+ * @property string $insurance_description
+ * @property string $insurance_percentage
+ *
+ * @property Record[] $records
+ */
+class Insurance extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'insurance';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['insurance_name'], 'required'],
+            [['insurance_percentage'], 'number'],
+            [['insurance_name', 'insurance_description'], 'string', 'max' => 45],
+            [['insurance_name'], 'unique']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'insurance_id' => 'Insurance ID',
+            'insurance_name' => 'Insurance Name',
+            'insurance_description' => 'Insurance Description',
+            'insurance_percentage' => 'Insurance Percentage',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRecords()
+    {
+        return $this->hasMany(Record::className(), ['insurance_id' => 'insurance_id']);
+    }
+}
