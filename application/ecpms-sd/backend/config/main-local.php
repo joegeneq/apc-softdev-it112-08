83e91d8e33449de4442e90c94cbc41041bb74ab8
Francis Horca <fdhorca@gmail.com>
2015-04-23 21:13:05 -0700

    added main-local and params-local

--- application/ecpms-sd/backend/config/main-local.php
+++ application/ecpms-sd/backend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'X6hJsOD_QOFFNAwuU56Wv2xV6rrYhCSG',
+            'cookieValidationKey' => 'Xbj5ew6isI7y-PlrrxRhJMChM5Knudnz',
         ],
     ],
 ];
