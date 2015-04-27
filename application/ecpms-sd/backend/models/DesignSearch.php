b4f7a308a9cc85fb0335c6deff304be33ffee33d
Francis Horca <fdhorca@gmail.com>
2015-04-27 18:04:34 +0800

    Design table crud

---
+++ application/ecpms-sd/backend/models/DesignSearch.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use backend\models\Design;
+
+/**
+ * DesignSearch represents the model behind the search form about `backend\models\Design`.
+ */
+class DesignSearch extends Design
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['design_id'], 'integer'],
+            [['design_name', 'design_description'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Design::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'design_id' => $this->design_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'design_name', $this->design_name])
+            ->andFilterWhere(['like', 'design_description', $this->design_description]);
+
+        return $dataProvider;
+    }
+}
