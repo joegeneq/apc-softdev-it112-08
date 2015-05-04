b23b0a3cadb4272c1b1c3c0d4013d7d1e06935d9
Jocelyn Cacao <joycacao@gmail.com>
2015-05-04 09:00:23 +0800

    Patient complex form of patient-profile and medical-history

---
+++ application/ecpms-sd/frontend/views/patient/index.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel frontend\models\PatientSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Patients';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="patient-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Patient', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            //'user_id',
+            'firstname',
+            'lastname',
+            'middlename',
+            //'birthday',
+             'gender',
+             'city_id',
+            // 'home_address',
+            // 'company_address',
+            // 'company_name',
+             'cel',
+             'tel',
+             'fb',
+            // 'sports:ntext',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
