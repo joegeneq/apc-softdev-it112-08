0b33b55050e2a96fe80510015db0a520c586ee13
Aeya Ramil <aeya.ramil@gmail.com>
2015-05-04 17:06:53 +0800

    Generate insurance CRUD

---
+++ application/ecpms-sd/frontend/views/insurance/index.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel frontend\models\InsuranceSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Insurances';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="insurance-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Insurance', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'insurance_name',
+            'insurance_description',
+            'insurance_percentage',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
