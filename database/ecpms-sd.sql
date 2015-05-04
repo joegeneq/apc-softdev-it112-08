3fa85c78204787ebdad503459e0eb5ed4f8549dc
Aeya Ramil <aeya.ramil@gmail.com>
2015-05-04 15:57:32 +0800

    Populate acuity table

--- database/ecpms-sd.sql
+++ database/ecpms-sd.sql
@@ -175,7 +175,21 @@ CREATE TABLE IF NOT EXISTS `ecpms-sd`.`acuity` (
   UNIQUE INDEX `acuity_name_UNIQUE` (`acuity_name` ASC))
 ENGINE = InnoDB;
 
-
+INSERT INTO `acuity` (`acuity_id`, `acuity_name`, `acuity_description`) VALUES
+(1, 'NLP', NULL),
+(2, 'LP', NULL),
+(3, 'HM', NULL),
+(4, '20/800', NULL),
+(5, '20/400', NULL),
+(6, '20/200', NULL),
+(7, '20/100', NULL),
+(8, '20/80', NULL),
+(9, '20/60', NULL),
+(10, '20/40', NULL),
+(11, '20/30', NULL),
+(12, '20/20', NULL),
+(13, '20/15', NULL),
+(14, '20/10', NULL);
 -- -----------------------------------------------------
 -- Table `ecpms-sd`.`patient_profile`
 -- -----------------------------------------------------
