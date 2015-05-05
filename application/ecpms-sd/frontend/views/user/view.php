b2498192fe3bac8eaf492ad20c822bb6541d8d0a
Jocelyn Cacao <joycacao@gmail.com>
2015-05-05 16:02:06 +0800

    redesigned user

--- application/ecpms-sd/frontend/views/user/view.php
+++ application/ecpms-sd/frontend/views/user/view.php
@@ -6,7 +6,7 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model frontend\models\User */
 
-$this->title = $model->id;
+//$this->title = $model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -16,13 +16,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
     </p>
 
     <?= DetailView::widget([
@@ -30,14 +23,14 @@ $this->params['breadcrumbs'][] = $this->title;
         'attributes' => [
             'id',
             'username',
-            'auth_key',
-            'password_hash',
+            //'auth_key',
+            //'password_hash',
             'password_reset_token',
             'email:email',
-            'role',
-            'status',
+            //'role',
+            //'status',
             'created_at',
-            'updated_at',
+            //'updated_at',
         ],
     ]) ?>
 
