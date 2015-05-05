ad4e11369b6aaf4b4f4c72546bff6486738bed0e
Jocelyn Cacao <joycacao@gmail.com>
2015-05-05 16:01:48 +0800

    redesigned patient profile

--- application/ecpms-sd/frontend/views/patient-profile/_form.php
+++ application/ecpms-sd/frontend/views/patient-profile/_form.php
@@ -1,7 +1,10 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
+use dosamigos\datepicker\DatePicker;
+use backend\models\City;
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\PatientProfile */
@@ -12,34 +15,45 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'user_id')->textInput() ?>
-
-    <?= $form->field($model, 'firstname')->textInput(['maxlength' => 255]) ?>
-
-    <?= $form->field($model, 'lastname')->textInput(['maxlength' => 255]) ?>
-
-    <?= $form->field($model, 'middlename')->textInput(['maxlength' => 255]) ?>
-
-    <?= $form->field($model, 'birthday')->textInput() ?>
-
-    <?= $form->field($model, 'gender')->textInput(['maxlength' => 1]) ?>
-
-    <?= $form->field($model, 'city_id')->textInput() ?>
-
-    <?= $form->field($model, 'home_address')->textInput(['maxlength' => 255]) ?>
-
-    <?= $form->field($model, 'company_address')->textInput(['maxlength' => 255]) ?>
-
-    <?= $form->field($model, 'company_name')->textInput(['maxlength' => 45]) ?>
-
-    <?= $form->field($model, 'cel')->textInput(['maxlength' => 45]) ?>
-
-    <?= $form->field($model, 'tel')->textInput(['maxlength' => 45]) ?>
-
-    <?= $form->field($model, 'fb')->textInput(['maxlength' => 75]) ?>
-
-    <?= $form->field($model, 'sports')->textarea(['rows' => 6]) ?>
-
+    <div style="display: none;">
+    <?= $form->field($model, 'user_id')->textInput(array('readonly' => true, 'value' => Yii::$app->user->identity->id)) ?>
+    </div>
+    <section style="margin: 10px;">
+        <fieldset>
+                <legend><b> Profile </b> </legend>
+            <center><table><tr><td style="width: 300px;"><?= $form->field($model, 'lastname') ?></td>
+                <td style="width: 300px;"><?= $form->field($model, 'firstname')->textInput(['maxlength' => 255]) ?></td>
+                <td style="width: 300px;"><?= $form->field($model, 'middlename')->textInput(['maxlength' => 255]) ?></td></tr>
+            </table></center>
+            <center><table>
+                <tr><td style="width: 180px;"><?= $form->field($model, 'birthday')->widget(
+                DatePicker::className(), [
+                    'inline' => false, 
+                    'clientOptions' => [
+                        'autoclose' => true,
+                        'format' => 'yyyy-mm-dd'
+                    ]
+                ]);?></td>
+                <td style="width: 180px;"><?= $form->field($model, 'gender')->radioList(array('M'=>'Male','F'=>'Female')); ?></td>
+                <td style="width: 180px;"><?= $form->field($model, 'cel')->textInput(['maxlength' => 45]) ?></td>
+                <td style="width: 180px;"><?= $form->field($model, 'tel')->textInput(['maxlength' => 45]) ?></td>
+                <td style="width: 180px;"><?= $form->field($model, 'fb')->textInput(['maxlength' => 75]) ?></td></tr>
+            </table></center>
+            <center><table>
+                <tr><td style="width: 900px;"><?= $form->field($model, 'sports')->textarea(['rows' => 6]) ?></td></tr>
+            </table></center>
+            <center><table>
+                <tr><td style="width: 300px;"><?php
+                    $cities=City::find()->all();
+                    $listData=ArrayHelper::map($cities, 'city_id', 'city_name');
+                    echo $form->field($model, 'city_id')->dropDownList(
+                        $listData,['prompt'=>'-- City --']);
+                ?></td>
+                <td style="width: 600px;"><?= $form->field($model, 'home_address')->textInput(['maxlength' => 255]) ?></td></tr>
+                <td style="width: 300px;"><?= $form->field($model, 'company_address')->textInput(['maxlength' => 255]) ?></td>
+                <td style="width: 600px;"><?= $form->field($model, 'company_name')->textInput(['maxlength' => 45]) ?></td>
+            </table></center>
+        </fieldset>
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
--- application/ecpms-sd/frontend/views/patient-profile/update.php
+++ application/ecpms-sd/frontend/views/patient-profile/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model frontend\models\PatientProfile */
 
-$this->title = 'Update Patient Profile: ' . ' ' . $model->user_id;
+//$this->title = 'Update Patient Profile: ' . ' ' . $model->user_id;
 $this->params['breadcrumbs'][] = ['label' => 'Patient Profiles', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->user_id, 'url' => ['view', 'id' => $model->user_id]];
 $this->params['breadcrumbs'][] = 'Update';
