4015a9cf791a53b22bd0e5704073edaba095f444
Jocelyn Cacao <joycacao@gmail.com>
2015-05-06 15:09:13 +0800

    record creation is disabled employee/admin should use diagnose(complex form)

--- application/ecpms-sd/frontend/views/record/create.php
+++ application/ecpms-sd/frontend/views/record/create.php
@@ -5,22 +5,15 @@ use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Record */
-
+/*
 $this->title = 'Create Record';
 $this->params['breadcrumbs'][] = ['label' => 'Records', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = $this->title;*/
 ?>
 <div class="record-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php 
-if(Yii::$app->user->identity->role == 10){
-	echo $this->render('/site/say');
-}else
-	echo $this->render('_form', [
-        'model' => $model,
-    ]) ?>
-    
+
 
 
 </div>
--- application/ecpms-sd/frontend/views/record/index.php
+++ application/ecpms-sd/frontend/views/record/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Record', ['create'], ['class' => 'btn btn-success']) ?>
+        
     </p>
 <?php
 if(Yii::$app->user->identity->role == 10){
