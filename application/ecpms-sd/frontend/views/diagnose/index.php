08ccd18e1bcb00fdeb0a966f0abeb57100297194
Jocelyn Cacao <joycacao@gmail.com>
2015-05-06 15:18:49 +0800

    only creation of diagnose is allowed this is the complex form for record, evaluation, prescription and accounting

--- application/ecpms-sd/frontend/views/diagnose/index.php
+++ application/ecpms-sd/frontend/views/diagnose/index.php
@@ -7,34 +7,13 @@ use yii\grid\GridView;
 /* @var $searchModel frontend\models\DiagnoseSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Diagnose';
-$this->params['breadcrumbs'][] = $this->title;
+/*$this->title = 'Diagnose';
+$this->params['breadcrumbs'][] = $this->title;*/
 ?>
 <div class="diagnose-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?= Html::a('Create Diagnose', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'record_id',
-            'patient_id',
-            'employee_id',
-            'patient_signature',
-            'agreed_date',
-            // 'employee_signature',
-            // 'complaints:ntext',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
 
 </div>
--- application/ecpms-sd/frontend/views/diagnose/update.php
+++ application/ecpms-sd/frontend/views/diagnose/update.php
@@ -5,17 +5,13 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Diagnose */
 
-$this->title = 'Update Diagnose: ' . ' ' . $model->record_id;
+/*$this->title = 'Update Diagnose: ' . ' ' . $model->record_id;
 $this->params['breadcrumbs'][] = ['label' => 'Diagnoses', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->record_id, 'url' => ['view', 'id' => $model->record_id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = 'Update';*/
 ?>
 <div class="diagnose-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
 
 </div>
