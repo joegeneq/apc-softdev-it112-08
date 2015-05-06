75914504082c55970dbb631e2856ecfc79da793b
Jocelyn Cacao <joycacao@gmail.com>
2015-05-06 15:05:41 +0800

    patient-profile creation is disabled employee/admin should use patient(complex form)

--- application/ecpms-sd/frontend/views/patient-profile/create.php
+++ application/ecpms-sd/frontend/views/patient-profile/create.php
@@ -6,20 +6,11 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model frontend\models\PatientProfile */
 
-$this->title = 'Create Patient Profile';
+/*$this->title = 'Create Patient Profile';
 $this->params['breadcrumbs'][] = ['label' => 'Patient Profiles', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = $this->title;*/
 ?>
 <div class="patient-profile-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
-<?php
-if(Yii::$app->user->identity->role == 10){
-    echo $this->render('/site/say');
-}else
-    echo  $this->render('_form', [
-			'model' => $model,
-        //'patient' => $patient,
-    ]) ?>
-
 </div>
--- application/ecpms-sd/frontend/views/patient-profile/index.php
+++ application/ecpms-sd/frontend/views/patient-profile/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Patient Profile', ['create'], ['class' => 'btn btn-success']) ?>
+        
     </p>
 <?php
 if(Yii::$app->user->identity->role == 10){
