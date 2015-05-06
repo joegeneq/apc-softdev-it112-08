f6ec4daa4af7e8b0f085c3ee692825e0f6ac43d9
Jocelyn Cacao <joycacao@gmail.com>
2015-05-06 15:13:48 +0800

    evaluation creation is disabled employee/admin should use diagnose(complex form)

--- application/ecpms-sd/frontend/views/evaluation/create.php
+++ application/ecpms-sd/frontend/views/evaluation/create.php
@@ -6,20 +6,13 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Evaluation */
 
-$this->title = 'Create Evaluation';
+/*$this->title = 'Create Evaluation';
 $this->params['breadcrumbs'][] = ['label' => 'Evaluations', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = $this->title;*/
 ?>
 <div class="evaluation-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-<?php
-if(Yii::$app->user->identity->role == 10){
-    echo $this->render('/site/say');
-}else
-    echo  $this->render('_form', [
-        'model' => $model,
-    ]) ?>
 
 </div>
--- application/ecpms-sd/frontend/views/evaluation/index.php
+++ application/ecpms-sd/frontend/views/evaluation/index.php
@@ -15,9 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?= Html::a('Create Evaluation', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+
 
  <?php
 if(Yii::$app->user->identity->role == 10){
