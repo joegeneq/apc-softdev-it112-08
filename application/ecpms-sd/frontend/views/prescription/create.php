f2b87842deb5791507c2fde65273b53fe614c918
Jocelyn Cacao <joycacao@gmail.com>
2015-05-06 15:13:59 +0800

    prescription creation is disabled employee/admin should use diagnose(complex form)

--- application/ecpms-sd/frontend/views/prescription/create.php
+++ application/ecpms-sd/frontend/views/prescription/create.php
@@ -6,20 +6,13 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Prescription */
 
-$this->title = 'Create Prescription';
+/*$this->title = 'Create Prescription';
 $this->params['breadcrumbs'][] = ['label' => 'Prescriptions', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = $this->title;*/
 ?>
 <div class="prescription-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-<?php
-if(Yii::$app->user->identity->role == 10){
-    echo $this->render('/site/say');
-}else
-    echo  $this->render('_form', [
-        'model' => $model,
-    ]) ?>
 
 </div>
