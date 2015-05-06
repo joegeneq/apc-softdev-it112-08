04f1a3f05e732c5d7ab9b539408969eee3f4c50b
Jocelyn Cacao <joycacao@gmail.com>
2015-05-06 15:07:30 +0800

    medical-history creation is disabled employee/admin should use patient(complex form)

--- application/ecpms-sd/frontend/views/medical-history/create.php
+++ application/ecpms-sd/frontend/views/medical-history/create.php
@@ -6,16 +6,14 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model frontend\models\MedicalHistory */
 
-$this->title = 'Create Medical History';
+/*$this->title = 'Create Medical History';
 $this->params['breadcrumbs'][] = ['label' => 'Medical Histories', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = $this->title;*/
 ?>
 <div class="medical-history-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+
 
 </div>
