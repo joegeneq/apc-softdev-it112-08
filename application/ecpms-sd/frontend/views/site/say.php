3b42e0ade10a048d7facdc758ddab13ef6176501
Jocelyn Cacao <joycacao@gmail.com>
2015-05-06 15:00:57 +0800

    error page with error image

---
+++ application/ecpms-sd/frontend/views/site/say.php
@@ -0,0 +1,9 @@
+<?php
+use yii\helpers\Html;
+?>
+<br><br><br><br>
+        <div id="display-error">
+         <img src="img/error.png" alt="Error" />
+         <p style="text-indent: 20px;">Ooops! This page is not allowed for you.</p>
+         <p style="text-indent: 55px;">Please use the menu for navigation.</p>
+        </div>
