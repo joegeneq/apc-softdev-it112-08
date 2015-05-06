66d73efa66ff6a31d78e8c7db49682fd505dd06d
Jocelyn Cacao <joycacao@gmail.com>
2015-05-06 15:13:38 +0800

    accounting creation is disabled employee/admin should use diagnose(complex form)

--- application/ecpms-sd/frontend/views/accounting/create.php
+++ application/ecpms-sd/frontend/views/accounting/create.php
@@ -6,20 +6,13 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Accounting */
 
-$this->title = 'Create Accounting';
+/*$this->title = 'Create Accounting';
 $this->params['breadcrumbs'][] = ['label' => 'Accountings', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = $this->title;*/
 ?>
 <div class="accounting-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-<?php
-if(Yii::$app->user->identity->role == 10){
-    echo $this->render('/site/say');
-}else
-    echo  $this->render('_form', [
-        'model' => $model,
-    ]) ?>
 
 </div>
